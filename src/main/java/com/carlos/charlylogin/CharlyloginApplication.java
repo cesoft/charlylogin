d0b07b9b002dcbab2ce1ef3f7cab39e4c472f57c
Carlos Espinoza <Carlos Espinoza@DESKTOP-5VSLPQ0>
2019-06-10 16:16:29 -0500

    login

--- src/main/java/com/carlos/charlylogin/CharlyloginApplication.java
+++ src/main/java/com/carlos/charlylogin/CharlyloginApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class CharlyloginApplication extends SpringBootServletInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(CharlyloginApplication.class);
